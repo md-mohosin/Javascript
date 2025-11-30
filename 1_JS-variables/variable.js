b33903b3fa0804793d8d787516d393983efeba9b
md mohosin <mdmohosin.web@gmail.com>
2025-11-30 22:24:26 +0600

    js variables

---
+++ 1_JS-variables/variable.js
@@ -0,0 +1,54 @@
+// VARIABLE DECLARE
+
+var c = 4
+console.log(c)
+
+let x = 5;
+let y = 6;
+
+console.log(typeof y)
+
+let z = x + y;
+console.log(z)
+
+
+const a = 10;
+const b = 22;
+console.log(a + b)
+
+
+let carName = 'Toyota';
+console.log(carName)
+console.log(typeof carName)
+
+const Price1 = 100;
+const Price2 = 50;
+
+let total = Price1 + Price2;
+console.log(total)
+
+
+g = 10; /* Bad practice */
+
+
+
+
+
+const cars = ['toyota', 'lamborgini', 'corolla']
+console.log(cars)
+cars[0] = 'tesla'
+console.log(cars)
+// cars = ['bmw','tata'] => Error
+console.log(typeof cars)
+
+
+
+
+const user = { name: 'Mohsin', age: 20 }
+console.log(user)
+user.age = 30
+console.log(user)
+user.role = 'Admin'
+// user = { name: 'Bilal', age: 34 } => Error
+console.log(user)
+console.log(typeof user)
