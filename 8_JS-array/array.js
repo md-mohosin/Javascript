ef87d361c03eca5ccfdae67812e261a1e4db693d
md mohosin <mdmohosin.web@gmail.com>
2025-12-05 23:12:01 +0600

    js array

---
+++ 8_JS-array/array.js
@@ -0,0 +1,78 @@
+const numbers = [12, 11, 33, 13, 44, 51]
+console.log(numbers)
+
+console.log(numbers[1])
+
+numbers[2] = 222
+numbers[0] = 111
+
+console.log(numbers)
+
+const numsLength = numbers.length;
+console.log(numsLength)
+
+const ages = []
+console.log(ages)
+
+
+
+
+
+const players = ['Babar', 'Rizwan', 'Fakhar', 'Shaheen']
+console.log(players)
+
+console.log(players.includes('Babar'))
+
+const includesPlayer = players.includes('Rizwan')
+console.log(includesPlayer)
+
+const includesPlayer2 = players.includes('Agha')
+console.log(includesPlayer2)
+
+console.log(players.indexOf('Rizwan'))
+
+const findIndex = players.indexOf('Haris')
+console.log(findIndex)
+
+const checkArry = Array.isArray(players)
+console.log(checkArry)
+
+const user = { name: "Mohsin", age: 20 }
+console.log(Array.isArray(user))
+
+
+
+
+
+const fruites = ['Apple','Mango','Orange','Guava','Lychee']
+
+console.log(fruites[2])
+
+fruites[2] = 'Jambura'
+
+console.log(fruites)
+
+
+
+
+
+
+const touristPlace = ["Cox's-bazar",'Sundarban','Bandarban']
+
+touristPlace.push('Himchari')
+
+touristPlace.push('Nilgiri','Jaflong')
+
+touristPlace.pop()
+
+console.log(touristPlace)
+
+
+
+
+
+const arr1 = ['Ahad','Bilal','Wahaj','Danish']
+const arr2 = ['Yumna','Dure','Dananeer','Sehar']
+
+const concatArray = arr1.concat(arr2)
+console.log(concatArray)
---
+++ 8_JS-array/push_and_pop.js
@@ -0,0 +1,52 @@
+const numbers = [11, 22, 33, 44, 55]
+console.log(numbers)
+
+numbers.push(444)
+numbers.push(111)
+numbers.push(222, 333, 666)
+console.log(numbers)
+
+
+
+
+const friends = ['Ahad', 'Bilal', 'Wahaj', 'Hamja', 'Danish']
+console.log(friends)
+
+friends.push('Murtasim')
+friends.push('Talha','Saad')
+console.log(friends)
+
+friends.pop()
+console.log(friends)
+
+friends.pop()
+
+const out1 = friends.pop()
+console.log(out1)
+
+console.log(friends)
+
+
+
+
+
+const fruites = ['Mango','Orange','Jackfruite','Lychee','Apple']
+console.log(fruites)
+
+fruites.shift()
+fruites.shift()
+console.log(fruites)
+
+fruites.unshift('Guava','Grapes')
+console.log(fruites)
+
+
+
+
+
+const players = []
+console.log(players)
+
+players.push('Babar Azam')
+players.push('Rizwan','Shaheen')
+console.log(players)
