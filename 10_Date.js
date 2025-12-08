c828ccbffd8bf24e3bcbd33f1f52cd88e9516ff3
md mohosin <mdmohosin.web@gmail.com>
2025-12-08 18:43:50 +0600

    Date system

---
+++ 10_Date.js
@@ -0,0 +1,24 @@
+console.log(Date())
+
+const today = new Date()
+console.log(today)
+console.log(today.getDay()) // index based day
+console.log('today date',today.getDate())
+
+
+const date = new Date('2062-12-9')
+console.log(date.getDay())
+console.log('month',date.getMonth()) //index based month
+console.log('Date',date.getDate())
+console.log(date.getFullYear())
+
+
+const specificDate = new Date(2091,11,12)
+console.log(specificDate)
+console.log(specificDate.getDate())
+console.log(specificDate.getMonth())
+specificDate.setMonth(5)
+console.log(specificDate)
+console.log(specificDate.toDateString())
+console.log(specificDate.toLocaleDateString())
+console.log(specificDate.toLocaleString('en-gb'))
