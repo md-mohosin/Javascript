15f1553545a3c40f6260886b93ec2b06ad6e124f
md mohosin <mdmohosin.web@gmail.com>
2025-12-12 08:50:03 +0600

    addEventListener

--- 12-DOM-events/js/event.js
+++ 12-DOM-events/js/event.js
@@ -1,8 +1,26 @@
-function makeGreen(){
-    document.body.style.backgroundColor='green'
+function makeGreen() {
+    document.body.style.backgroundColor = 'green'
 }
 
 document.getElementById('makeRed').onclick = makeRed
-function makeRed(){
-    document.body.style.backgroundColor='red'
+function makeRed() {
+    document.body.style.backgroundColor = 'red'
 }
+
+
+const makePnurple = document.getElementById('makePurple')
+makePnurple.addEventListener("click",purple)
+function purple(){
+    document.body.style.backgroundColor='purple'
+}
+
+
+const makePink = document.getElementById("makePink")
+makePink.addEventListener("click", function () {
+    document.body.style.backgroundColor = 'pink'
+})
+
+
+document.getElementById('makeBlue').addEventListener("mouseover",function(){
+    document.body.style.backgroundColor='blue'
+})
