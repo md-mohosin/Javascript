df419f66b90c7544df2f5ffe600bcfeab7f038bb
md mohosin <mdmohosin.web@gmail.com>
2025-12-12 08:16:04 +0600

    onclick event

---
+++ 12-DOM-events/js/event.js
@@ -0,0 +1,8 @@
+function makeGreen(){
+    document.body.style.backgroundColor='green'
+}
+
+document.getElementById('makeRed').onclick = makeRed
+function makeRed(){
+    document.body.style.backgroundColor='red'
+}
