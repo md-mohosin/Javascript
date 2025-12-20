190f16112cd9c37e98fc19a1fed57a1b4e053e9a
md mohosin <mdmohosin.web@gmail.com>
2025-12-20 11:52:51 +0600

    JS math

---
+++ 15-Math.js
@@ -0,0 +1,67 @@
+console.log(Math.E)
+
+console.log(Math.round(4.7))
+console.log(Math.round(4.5))
+console.log(Math.round(4.3))
+
+
+console.log(Math.ceil(5.2))
+console.log(Math.ceil(5.9))
+console.log(Math.ceil(-5.3))
+
+
+console.log(Math.floor(2.3))
+console.log(Math.floor(2.8))
+console.log(Math.floor(-2.8))
+
+
+console.log(Math.trunc(3.9))
+console.log(Math.trunc(3.1))
+console.log(Math.trunc(-3.1))
+
+
+console.log(Math.sign(7))
+console.log(Math.sign(-7))
+
+
+console.log(Math.pow(6, 2))
+console.log(Math.pow(4, 3))
+console.log(Math.pow(3, 5))
+
+
+console.log(Math.sqrt(8))
+console.log(Math.sqrt(9))
+
+
+console.log(Math.abs(10))
+console.log(Math.abs(-10))
+
+
+console.log(Math.max(22, 44, 77, 11, 33))
+console.log(Math.min(22, 44, 77, 11, 33))
+
+
+
+const nums = [5, 33, 99, 22, 55, 44]
+
+console.log(Math.max(...nums))
+console.log(Math.min(...nums))
+
+
+console.log(Math.random())
+console.log(Math.random() * 10)
+console.log(Math.random() * 2)
+console.log(Math.random() + 1)
+console.log(Math.random() * 2 + 1)
+
+console.log(Math.log(1))
+console.log(Math.log(5))
+
+
+console.log(Math.log2(4))
+console.log(Math.log2(8))
+
+console.log(Math.log10(3))
+console.log(Math.log10(7))
+
+
